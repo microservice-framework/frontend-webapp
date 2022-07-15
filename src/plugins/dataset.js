2ba28e49c10f44d8132a4a925921dba0afc7c61b
Gor Martsen <gor.martsen@gmail.com>
2022-07-15 14:51:51 -0400

    data set working prototype 1

--- src/plugins/dataset.js
+++ src/plugins/dataset.js
@@ -1,9 +1,11 @@
 import { reactive } from "vue";
 
-function DataSetState(setDefenition) {
+function DataSetState(app, setDefenition) {
   this.setDefenition = setDefenition;
+  this.app = app;
   var self = this;
-  self.dataset = {};
+  self._dataset = {};
+  
   for (var dataset in this.setDefenition) {
     var newValue = self.getItem(dataset);
     var definition = self.setDefenition[dataset];
@@ -18,9 +20,61 @@ function DataSetState(setDefenition) {
         break;
       }
     }
-    self.dataset[dataset] = newValue;
+    var _refresh = function(){
+      if(definition.provider) {
+        //var _self = this;
+        definition.provider(self.app, function(update){
+          //_self.value = update;
+          self.app['$dataset'][dataset] = reactive(update);
+          self._dataset[dataset]['refresh'] = _refresh;
+        })
+      }
+    }
+    self._dataset[dataset] = reactive(newValue);
+    self._dataset[dataset]['refresh'] = _refresh
   }
+  this.dataset = new Proxy(this._dataset, {
+    set: function(obj, prop, value) {
+      var definition = self.setDefenition[prop];
+      var _refresh = function(){
+        if(definition.provider) {
+          //var _self = this;
+          definition.provider(self.app, function(update){
+            //_self.value = update;
+            obj[prop] = reactive(update);
+            obj[prop]['refresh'] = _refresh;
+          })
+        }
+      }
+      obj[prop] = reactive(value);
+      obj[prop]['refresh'] = _refresh;
+      return true;
+    }
+  })
   return self;
+}
+
+DataSetState.prototype.refresh = function (dataset) {
+  var definition = this.setDefenition[dataset];
+  var self = this;
+  if(definition.provider) {
+    var _refresh = function(){
+      if(definition.provider) {
+        //var _self = this;
+        definition.provider(self.app, function(update){
+          //_self.value = update;
+          self._dataset[dataset] = reactive(update);
+          self._dataset[dataset]['refresh'] = _refresh;
+        })
+      }
+    }
+    definition.provider(self.app, function(update){
+      self._dataset[dataset] = update;
+      self._dataset[dataset]['refresh'] = _refresh;
+    })
+    return true;
+  }
+  return false;
 }
 
 /**
@@ -28,53 +82,43 @@ function DataSetState(setDefenition) {
  */
 DataSetState.prototype.getItem = function (dataset) {
   var definition = this.setDefenition[dataset];
-  var value;
-  if (!definition) {
-    return undefined;
-  }
+  var self = this;
+  var defaultValue
 
-  if (typeof value !== "string") {
-    // No data stored yet
-    if (typeof definition.default !== "undefined") {
-      return definition.default;
-    }
-    return undefined;
-  }
-  switch (definition.type) {
-    case "boolean": {
-      return value == "true";
-    }
-    case "number": {
-      return parseInt(value);
-    }
-    case "float": {
-      return parseFloat(value);
-    }
-    case "array": {
-      try {
-        return JSON.parse(value);
-      } catch (e) {
-        console.error("ARRAY: failed to parse ", value);
-        return [];
+  if (typeof definition.default !== "undefined") {
+    defaultValue = definition.default;
+  } else {
+    switch (definition.type) {
+      case "boolean": {
+        defaultValue = false;
+        break;
       }
-    }
-    case "object": {
-      try {
-        return JSON.parse(value);
-      } catch (e) {
-        console.error("Object: failed to parse ", value);
-        return {};
+      case "number": {
+        defaultValue = 0;
+        break;
+      }
+      case "float": {
+        defaultValue = 0.0;
+        break;
+      }
+      case "array": {
+        defaultValue = [];
+        break;
+      }
+      case "object": {
+        defaultValue = {};
+        break;
       }
     }
   }
-  return value;
-};
+  return defaultValue;
+}
 
 export default {
   install: (app, options) => {
     app._DataSetState = false;
     // inject a globally available $translate() method
-    var datasetStorage = new VuePersistState(options);
+    var datasetStorage = new DataSetState(app, options);
     app.config.globalProperties["$dataset"] = reactive(datasetStorage.dataset);
     app.mixin({
       created: function beforeCreate() {
