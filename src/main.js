f7a877a27b39ee95bb13286b46f22d1a6144f663
Gor Martsen <gor.martsen@gmail.com>
2022-07-19 12:49:32 -0400

    add debug and cleanup

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from "vue";
 import PersistState from "vue-persist-state";
 import DatasetState from "@gormartsen/vue-dataset";
 import ApiClient from "@microservice-framework/vue-api-client";
+import Debug from "@gormartsen/vue-debug";
 
 import App from "./App.vue";
 import router from "./router";
@@ -14,6 +15,7 @@ import defaultDataSet from "./settings/dataset-settings";
 
 var sessionName = "API-Browser-APP";
 
+window.APP.use(Debug, true);
 window.APP.use(router);
 window.APP.use(PersistState, sessionName, defaultSettings);
 window.APP.use(DatasetState, defaultDataSet);
